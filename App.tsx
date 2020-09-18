cc288a7c56ec6c6786a7643829d2b28a3ddf734b
Shiyu Cheng <starkeyshiyu@gmail.com>
2020-09-17 18:34:41 -0700

    pref: initial project

---
+++ App.tsx
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text} from 'react-native';
+import Config from 'react-native-config';
+
+declare const global: {HermesInternal: null | {}};
+
+const App = () => {
+  return (
+    <>
+      <Text>HELLO</Text>
+      <Text>{Config.API_URL}</Text>
+    </>
+  );
+};
+
+export default App;
